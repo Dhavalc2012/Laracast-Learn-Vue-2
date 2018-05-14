0333e26a5a099046d804af3399bbbd47a50a3db4
Dhaval <dhavalchheda@ymail.com>
2018-05-14 18:56:40 +0530

    Laracast Learn Vue 2 Chapter 8 Components within components.

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+Vue.component('task-list',{
+    template:`<div>
+    <li v-for="task in tasks">{{task.task}}</li>
+    </div>`,
+    data(){
+        return {
+            tasks : [
+                {
+                    task : 'Go to the store',
+                    complete : true
+                },
+                {
+                    task : 'Complete the website',
+                    complete : false
+                },
+                {
+                    task : 'Change the designing',
+                    complete : false
+                },
+                {
+                    task : 'Map up the task',
+                    complete : true
+                },
+            ]
+        }
+    }
+});
 Vue.component('task',{
     template:'<li><slot></slot></li>'
 });
