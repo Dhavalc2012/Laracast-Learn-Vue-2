5e2275b7c52beb4b9b906c804a01ca5acaaea187
Dhaval <dhavalchheda@ymail.com>
2018-05-14 19:59:43 +0530

    Laracast Learn Vue 2 Chapter 10 Practical Component Exercise 2 Modal

--- main.js
+++ main.js
@@ -1,26 +1,23 @@
-Vue.component('message',{
-    props : ['title','body'],
-    data(){
-        return {
-            isVisible : true
-        }
-    },
+Vue.component('modal',{
     template : `
-    <article class="message" v-show="isVisible">
-                    <div class="message-header">
-                     {{title}}    <button class="delete" @click="hideModal" aria-label="delete"></button>
-                    </div>
-                    <div class="message-body">
-                    {{body}}
-                    </div>
-                  </article>        
-    `,
-    methods: {
-        hideModal(){
-this.isVisible = false;
-        }
-    }
+    <div class="modal is-active">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+        <div class="box">
+
+       
+      <p>
+         <slot></slot>
+      </p>
+    </div>
+    </div>
+    <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+  </div> 
+    `
 });
 new Vue({
-    el:'#root'
+    el:'#root',
+    data : {
+        showModal : false
+    }
 });
