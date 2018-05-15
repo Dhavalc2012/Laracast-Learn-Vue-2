ada1f3065550a4b868b32bac12db0bbf4fb34e80
Dhaval <dhavalchheda@ymail.com>
2018-05-15 13:05:27 +0530

    Laracast Learn Vue 2 Chapter 13 Component Communication Example #2 : Custom Events

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
+//window.Event = new Vue();
+
+window.Event = new class{
+    constructor(){
+        this.vue = new Vue();
+    }
+    fire(event, data=null){
+            this.vue.$emit(event,data);
+    }
+    listen(event, callback){
+            this.vue.$on(event,callback);
+    }
+}
 Vue.component('coupon',{
     template : '<input placeholder="Enter your coupon code" @blur="onCouponApplied">',
     methods : {
         onCouponApplied(){
-            this.$emit('applied');
+           // Event.$emit('applied');
+           Event.fire('applied');
         }
     }
 });
@@ -13,11 +27,15 @@ new Vue({
     data : {
      couponApplied : false
     },
-    methods :{
-        onCouponApplied(){
-            this.couponApplied = true;
-        }
-    }
+  created(){
+    //   Event.$on('applied',() => {
+    //       alert("Handle the event");
+    //   })
+
+    Event.listen('applied',() => {
+              alert("Handle the event");
+           });
+  }
     
 });
 
